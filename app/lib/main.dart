41bd5c089e63b0e8682a887e0d7e8f1566be236e
Brian Uptagrafft <upta@outlook.com>
2021-08-03 17:42:51 -0700

    Init firebase. Trying out (guessing) github actions to deploy to host

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,14 +1,11 @@
+import 'package:dashboard/view/app.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() {
-  runApp(const App());
-}
-
-class App extends StatelessWidget {
-  const App({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp();
-  }
+  runApp(
+    const ProviderScope(
+      child: App(),
+    ),
+  );
 }
