b5b1dad1c9d2f9a0fa4dc8928081b75dbdb60966
Brian <upta@outlook.com>
2021-09-04 14:01:57 -0700

    Upgraded /rules to firebase jdk 9.0. Added storage + tests (#5)
    * Upgraded /rules to firebase jdk 9.0. Added storage + tests
    
    * Removed xhr2 pkg that I added during dev but didn't end up needing
    
    * Don't detectOpenHandles so it doesn't hang
    
    * Force exit

--- rules/src/helpers.js
+++ rules/src/helpers.js
@@ -1,82 +1,102 @@
-const firebase = require("@firebase/rules-unit-testing");
-const fs = require("fs");
+const {
+  initializeTestEnvironment,
+  assertFails,
+  assertSucceeds,
+} = require("@firebase/rules-unit-testing");
+const admin = require("firebase-admin");
 
-module.exports.setup = async (auth, data) =>
-{
-    const projectId = "fiobabig-dashboard";
+const projectId = "fiobabig-dashboard";
 
-    process.env.FIRESTORE_EMULATOR_HOST = "localhost:8080";
-    
-    const admin = await firebase.initializeAdminApp({
-        projectId,
-    });
+process.env.FIRESTORE_EMULATOR_HOST = "localhost:8080";
 
-    const app = await firebase.initializeTestApp({
-        projectId,
-        auth
-    });
-
-    const db = admin.firestore();
-    
-    if (data)
-    {
-        for (const key in data)
-        {
-          const ref = db.doc(key);
-
-          try{
-              await ref.set(data[key]);
-          }
-          catch(err){
-            console.log(err, "This is me");
-          }
-        }
-    }
-
-    return app.firestore();
-};
-
-module.exports.teardown = async () =>
-{
-    await Promise.all(firebase.apps().map(a=> a.delete()))
-};
-
-expect.extend(
-{
-    async toAllow(x)
-    {
-        let pass = false;
-
-        try
-        {
-            await firebase.assertSucceeds(x);
-            pass = true;
-        }
-        catch (err) {}
-
-        return {
-            pass,
-            message: () => "Expected Firebase operation to be allowed, but it was denied"
-        };
-    }
+const adminApp = admin.initializeApp({
+  projectId,
 });
 
-expect.extend(
-{
-    async toDeny(x)
-    {
-        let pass = false;
-        
-        try
-        {            
-            await firebase.assertFails(x);
-            pass = true;
-        }
-        catch (err) { console.log(err) }
+(function hidePermissionDeniedWarnings() {
+  const realConsole = console;
 
-        return {
-            pass,
-            message: () => "Expected Firebase operation to be denied, but it was allowed"
-        };
+  global.console = {
+    ...realConsole,
+    warn: (...data) => {
+      if (data.length === 1) {
+        if (
+          data.toString().includes("@firebase/firestore") &&
+          data.toString().includes("PERMISSION_DENIED:")
+        ) {
+          return;
+        }
+
+        realConsole.warn(data);
+      }
+    },
+  };
+})();
+
+module.exports.setup = async (uid, data) => {
+  const db = adminApp.firestore();
+
+  if (data) {
+    for (const key in data) {
+      const ref = db.doc(key);
+
+      try {
+        await ref.set(data[key]);
+      } catch (err) {
+        console.log(err, "This is me");
+      }
     }
+  }
+
+  const env = await initializeTestEnvironment({
+    projectId,
+    storage: {
+      host: "localhost",
+      port: 9199,
+    }, // why does this automatically pick up firestore but not storage? both emulators are defined in the firebase.json
+  });
+
+  if (uid) {
+    return env.authenticatedContext(uid);
+  } else {
+    return env.unauthenticatedContext();
+  }
+};
+
+module.exports.teardown = async () => {
+  await Promise.all(firebase.apps().map((a) => a.delete())); // this maybe shouldn't exist/be done differently.  don't care atm
+};
+
+expect.extend({
+  async toAllow(x) {
+    let pass = false;
+
+    try {
+      await assertSucceeds(x);
+      pass = true;
+    } catch (err) {}
+
+    return {
+      pass,
+      message: () =>
+        "Expected Firebase operation to be allowed, but it was denied",
+    };
+  },
+});
+
+expect.extend({
+  async toDeny(x) {
+    let pass = false;
+
+    try {
+      await assertFails(x);
+      pass = true;
+    } catch (err) {}
+
+    return {
+      pass,
+      message: () =>
+        "Expected Firebase operation to be denied, but it was allowed",
+    };
+  },
 });
